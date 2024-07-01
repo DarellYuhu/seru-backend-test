b342069414bbf7610df3348a4afd71e60311967f
Darell Mona <s2200064@student.unklab.ac.id>
2024-07-01 21:25:50 +0800

    setup project

---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+export class Config {
+  public static PORT = Bun.env.PORT;
+  public static DATABASE_URL = Bun.env.DATABASE_URL;
+}
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import app from "./app";
+import { Config } from "./config";
+
+const server = Bun.serve({
+  fetch: app.fetch,
+  port: Config.PORT,
+});
+
+console.log(`Server is running on port: ${server.port}`);
